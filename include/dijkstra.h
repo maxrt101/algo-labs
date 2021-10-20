fe593dcc5f68c62b271cfe2b5ec28b1c8feb1d85
Maksym Tkachuk <s2m4f6@gmail.com>
2021-10-20 16:31:30 +0300

    Implemented Dijkstra's algorithm & tests for it

---
+++ include/dijkstra.h
@@ -0,0 +1,66 @@
+/**
+ * Dijkstra's algorith
+ *
+ * For a given source node in the graph, the algorithm finds the shortest path between that node and every other node.
+ * Graph is represented by a 2D vector.
+ * 
+ * @file   dijkstra.h
+ * @author maxrt101
+ */
+
+
+#ifndef _DIJKSTRA_H
+#define _DIJKSTRA_H
+
+#include <vector>
+#include <limits>
+#include <cstdint>
+
+template <typename T>
+using Graph = std::vector<std::vector<T>>;
+
+template <typename T>
+inline int dijkstraMinimalDistance(const std::vector<T>& dist, const std::vector<bool>& visited) {
+  const T MAX_T = std::numeric_limits<T>::max();
+
+  int minimum = MAX_T, index;
+
+  for (int i = 0; i < dist.size(); i++) {
+    if (!visited[i] && dist[i] <= minimum) {
+      minimum = dist[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
+template <typename T, typename DistanceType = int>
+inline std::vector<T> dijkstra(Graph<T> graph, DistanceType src) {
+  const T MAX_T = std::numeric_limits<T>::max();
+
+  std::vector<T> dist(graph.size());
+  std::vector<bool> visited(graph.size());
+  
+  for (int i = 0; i < dist.size(); i++) {
+    dist[i] = MAX_T;
+    visited[i] = false;
+  }
+
+  dist[src] = 0;
+
+  for (int i = 0; i < dist.size(); i++) {
+    int m = dijkstraMinimalDistance(dist, visited);
+    visited[m] = true;
+    for (int i = 0; i < dist.size(); i++) {
+      if (!visited[i] && graph[m][i]
+       && dist[m] != MAX_T
+       && dist[m] + graph[m][i] < dist[i]) {
+        dist[i] = dist[m] + graph[m][i];
+      }
+    }
+  }
+  return dist;
+}
+
+#endif
+
---
+++ src/main.cc
@@ -0,0 +1,50 @@
+/**
+ * Dijkstra's algorithm tests
+ *
+ * @file   main.cc
+ * @author maxrt101
+ */
+
+#include <mrt/test/framework.h>
+#include <mrt/args/args.h>
+
+#include "dijkstra.h"
+
+#include <iostream>
+
+int main(int argc, char ** argv) {
+  mrt::test::TestFramework testFramework;
+
+  testFramework.addTest({"Test 1", []() -> bool {
+    Graph<int> g {
+      {0, 1, 2, 0, 0, 0},
+      {1, 0, 0, 5, 1, 0},
+      {2, 0, 0, 2, 3, 0},
+      {0, 5, 2, 0, 2, 2},
+      {0, 1, 3, 2, 0, 1},
+      {0, 0, 0, 2, 1, 0}
+    };
+
+    const std::vector<int> expected {0, 1, 2, 4, 2, 3};
+    auto result = dijkstra(g, 0);
+
+    return expected == result;
+  }});
+
+  testFramework.addTest({"Test 2", []() -> bool {
+    Graph<int> g {
+      {0, 3, 1, 0, 0},
+      {3, 0, 7, 5, 1},
+      {1, 7, 0, 2, 0},
+      {0, 5, 2, 0, 7},
+      {0, 1, 0, 7, 0}
+    };
+    
+    const std::vector<int> expected {1, 4, 0, 2, 5};
+    auto result = dijkstra(g, 2);
+
+    return expected == result;
+  }});
+
+  return !testFramework.run(true);
+}
